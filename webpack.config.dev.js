4441cc73bbf33e6c49859d5e0778f4a01b0c1454
Denis Aksiutsik <denis@Deniss-MacBook-Pro.local>
2017-05-22 13:27:18 -0700

    Project setup: Hot reloading setup

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,18 +1,40 @@
 import path from 'path';
+import webpack from 'webpack';
 
 export default {
-  entry: path.join(__dirname, '/client/index.js'),
+  entry: [
+  'webpack-hot-middleware/client',
+  path.join(__dirname, '/client/index.js')
+  ],
   output: {
     filename: 'bundle.js',
-    path: '/'
+    path: '/',
+    publicPath: '/'
   },
+  plugins: [
+  new webpack.NoEmitOnErrorsPlugin(),
+  new webpack.optimize.OccurrenceOrderPlugin(),
+  new webpack.HotModuleReplacementPlugin()
+  ],
   module: {
     loaders: [
-      {
-        test: /\.js$/,
-        include: path.join(__dirname, 'client'),
-        loaders: [ 'babel-loader' ]
-      },
+    {
+      test: /\.js$/,
+      include: path.join(__dirname, 'client'),
+      loaders: [ 'react-hot-loader', 'babel-loader' ]
+    },
+    {
+      test: /\.scss$/,
+      loaders: ['style-loader', 'css-loader', 'sass-loader']
+    },
+    {
+      test: /\.(?:png|jpg|svg)$/,
+      loader: 'url-loader',
+      query: {
+        // Inline images smaller than 10kb as data URIs
+        limit: 10000
+      }
+    }
     ]
   },
   resolve: {
